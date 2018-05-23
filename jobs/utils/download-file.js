63c7bb60594bac5dd205e8c6e1d83200f3373da8
Curtis Cummings <curtis@aquil.io>
2018-05-22 22:25:47 -0400

    Decrease memory footprint of downloading files.
    Current implementation used `download`'s Promise API which is very inefficient with memory.
    New implementation uses the duplex stream returned by `download` piping it into a writable stream
    before, optionally, decompressing the file.

--- jobs/utils/download-file.js
+++ jobs/utils/download-file.js
@@ -5,22 +5,43 @@
  */
 
 const path = require('path');
+const fse = require('fs-extra');
+const fs = require('fs');
 const download = require('download');
-const mime = require('mime-types');
+const decompress = require('decompress');
 
-module.exports = function({
+module.exports = async function ({
   url, dest
 }) {
-  const extension = path.extname(url);
-  const options = {
-    strip: 1,
-    mode: '666',
-    headers: {
-      accept: mime.lookup(extension)
+  const { base, ext } = path.parse(url);
+  const fileName = `${dest}/${base}`;
+  await fse.ensureFile(fileName);
+  return new Promise(async (resolve, reject) => {
+    // Memory constraints are a real concern here. Using 
+    // `download({ decompress: true })` stores everything in memory which is 
+    // very inefficient.
+    //
+    // `download` returns a duplex stream so we can pipe to a writebale stream
+    // to drastically cut down on memory footprint.
+    //
+    // Decompressing files can be done using streams theoretically. Many of 
+    // decompress' plugins accept streams but decompress itself does not.
+    // See: https://github.com/kevva/decompress/issues/29
+    //
+    // For now we download the archive and then decompress.
+    if (['.zip', '.gz', '.xz', '.tar', '.bz2'].includes(ext)) {
+      await download(url).pipe(fs.createWriteStream(fileName)).on('finish', async () => {
+        await decompress(fileName, dest, {
+          strip: 1
+        }).catch(e => reject(e));
+        fse.removeSync(fileName);
+        resolve();
+      })
+      .on('error', e => reject(e));
+    } else {
+      download(url).pipe(fs.createWriteStream(fileName))
+        .on('finish', () => resolve())
+        .on('error', e => reject(e))
     }
-  };
-  if(['.zip', '.gz', '.tar', '.bz2'].includes(extension)) {
-    options.extract = true;
-  }
-  return download(url, dest, options);
+  });
 };
